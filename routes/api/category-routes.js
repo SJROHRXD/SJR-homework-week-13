b6678b6373e5016cb38665641bbd54431f670497
SJRXD <rohr.olasz@gmail.com>
2021-11-13 20:38:56 -0800

    built models

---
+++ routes/api/category-routes.js
@@ -0,0 +1,54 @@
+const router = require('express').Router();
+const { Category, Product } = require('../../models');
+const { findByPk } = require('../../models/Product');
+
+// The `/api/categories` endpoint
+// http://localhost:3001/api/categories
+
+  // find all categories
+  // be sure to include its associated Products
+router.get('/', async (req, res) => {
+try {
+  const categoryData = await Category.findAll()
+    res.status(200).json(categoryData);
+    // console.log("Passing categoryData")
+  } catch (err) {
+  res.status(500).json(err)
+  }
+});
+
+  // find one category by its `id` value
+  // be sure to include its associated Products
+router.get('/:id', async (req, res) => {
+  try {
+    // request parameter id; feeding ID and searching DB for match
+    const categoryData = await Category.findByPk(req.params.id, {
+      // including any children Products; for example sports will return associated sports products
+      include: [{ model: Product }]
+    })
+    if (!categoryData) {
+      // if there's no matching category, then error message
+      res.status(404).json({ message: "NO DATA FOR THIS ID🧁✨🐱‍👤" })
+      return;
+    }
+    // otherwise, returns data
+    res.status(200).json(categoryData)
+  } catch (err) {
+  res.status(500).json(err)
+  // display error for server
+  }
+});
+
+router.post('/', (req, res) => {
+  // create a new category
+});
+
+router.put('/:id', (req, res) => {
+  // update a category by its `id` value
+});
+
+router.delete('/:id', (req, res) => {
+  // delete a category by its `id` value
+});
+
+module.exports = router;
